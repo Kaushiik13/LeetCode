743393b94b441d6bce7033df455c26fdd0417855
Kaushiik A <akkaushiik@gmail.com>
2024-08-06 14:53:22 +0530

    [1] [Time Beats: 7.83%] [Memory Beats: 26.69%] - LeetPush

---
+++ Two Sum/1-Two-Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int a = nums[i] + nums[j];
+                if (a == target) {
+                    System.out.printf(\We return [%d, %d]%n\, nums[i], nums[j]);
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {}; // Return an empty array if no solution is found
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println(\Indices: [\ + result[0] + \, \ + result[1] + \]\);
+    }
+}

3290fd533eef09ab91fd107da1b57abe45603229
Kaushiik A <akkaushiik@gmail.com>
2024-08-10 22:05:01 +0530

    Create TwoSum.java

---
+++ Two Sum/TwoSum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int a = nums[i] + nums[j];
+                if (a == target) {
+                    System.out.printf("We return [%d, %d]%n", nums[i], nums[j]);
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {}; // Return an empty array if no solution is found
+    }
+}
